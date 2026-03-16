2a22b422492b35636e5d514009c7df01b78a94ba
VibeCraft <uskovmaxim12@gmail.com>
2026-03-16 16:31:17 +0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/global.css';
+import './styles/global.css'; // убедитесь, что путь правильный
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
